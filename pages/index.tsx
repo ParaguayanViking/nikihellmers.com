c554098a403764214d397d7242bc815b64a172cf
Nicholas Hellmers <nihe1661@colorado.edu>
2021-12-10 17:30:51 -0700

    Logo Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,9 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.header}>
           <div className={styles.menu}>
-            <div className={styles.menu_logo}>
-              <a href="">
-                <h3>LOGO</h3>
-              </a>
-            </div>
+            <a href="">
+            <div className={styles.menu_logo}></div>
+            </a>
             <div className={styles.menu_items}>
               <a href="#">Portfolio</a>
               <a href="#">Contact</a>
