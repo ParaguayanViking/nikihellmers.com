739190309aba5566f2fe74f559e9523ae1b8d5e3
Nicholas Hellmers <nihe1661@colorado.edu>
2022-01-04 18:05:49 -0700

    added MySQL to project

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import ProjectDisplayer from '../components/ProjectWrapper/ProjectDisplayer'
 import Menu from '../components/Menu/Menu'
 
 const Portfolio: NextPage = () => {
     return (
-        <main>
-            <Menu/>
-            <ProjectDisplayer/>
-        </main>
+        <div>
+            <Head>
+            <title>Niki Hellmers - Portfolio</title>
+            <meta name="description" content="Niki Hellmers Personal Website" />
+            <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <Menu/>
+                <ProjectDisplayer/>
+            </main>
+        </div>
     )
   }
   
