2a2e223905a9451ddeba94ee47d81ab8ee408a98
Nicholas Hellmers <nihe1661@colorado.edu>
2022-01-05 20:45:14 -0700

    Created Footer Component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import styles from 'Footer.module.css'
+
+function Footer() {
+    <div className={styles.footerWrapper}>
+        <div>
+            <h1>Section 1</h1>
+        </div>
+        <div>
+            <h1>Section 2</h1>
+        </div>
+        <div>
+            <h1>Section 3</h1>
+        </div>
+        <div>
+            <h1>Section 3</h1>
+        </div>
+    </div>
+}
+
+export default Footer
