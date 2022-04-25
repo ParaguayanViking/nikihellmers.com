526d84572151d486935254235f9803c918574481
ParaguayanViking <nicholashellmers@Nicholass-MacBook-Pro-2.local>
2022-04-24 22:28:22 -0600

    change in known techs svgs

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return(
         <div className={styles.footerWrapper}>
             <div>
-                
+                Hello
             </div>
         </div>
     )
--- components/QuickKnownTechs/QuickKnownTechs.tsx
+++ components/QuickKnownTechs/QuickKnownTechs.tsx
@@ -5,27 +5,17 @@
 
 */
 import styles from './QuickKnownTechs.module.css'
-  
 function QuickKnownTechs() {
   return (
     <div className={styles.wrapper}>
-        <h2>Some of My Expertees include</h2>
+        <h2>Some of My Expertise Include</h2>
         <div className={styles.list}>
-            <ul>
-                <li>JavaScript/TypeScript</li>
-                  <li>||</li>
-                <li>C++</li>
-                  <li>||</li>
-                <li>React/NEXTjs</li>
-                  <li>||</li>
-                <li>HTML</li>
-                  <li>||</li>
-                <li>CSS</li>
-                  <li>||</li>
-                <li>Python</li>
-                  <li>||</li>
-                <li>Pytorch</li>
-            </ul>
+            <a id={styles["cpp"]} className={styles.box} href="https://en.wikipedia.org/wiki/C%2B%2B" target="_blank"></a>
+            <a id={styles["js"]} className={styles.box} href="https://en.wikipedia.org/wiki/JavaScript" target="_blank"></a>
+            <a id={styles["njs"]} className={styles.box} href="https://en.wikipedia.org/wiki/next.js" target="_blank"></a>
+            <a id={styles["py"]} className={styles.box} href="https://en.wikipedia.org/wiki/Python_(programming_language)" target="_blank"></a>
+            <a id={styles["react"]} className={styles.box} href="https://en.wikipedia.org/wiki/React_(JavaScript_library)" target="_blank"></a>
+            <a id={styles["ts"]} className={styles.box} href="https://en.wikipedia.org/wiki/TypeScript" target="_blank"></a>
         </div>
     </div>
   )
